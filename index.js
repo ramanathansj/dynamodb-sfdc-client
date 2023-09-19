2468cf52d56eaf33230ebd0f9aae71c27b4f034c
ramanathan <rpachaiyappan@gmail.com>
2023-09-18 18:49:43 -0700

    DynamoDB -> SFDC REST Client (Common for both Async Request-Reply and Pub/Sub pattern)
    DynamoDB -> SFDC REST Client (Common for both Async Request-Reply and Pub/Sub pattern)

---
+++ index.js
@@ -0,0 +1,65 @@
+/**
+ * @description       : 
+ * @author            : Ramanathan
+ * @group             : 
+ * @last modified on  : 09-18-2023
+ * @last modified by  : Ramanathan
+**/
+
+import SFConnect from "./sfconnect.js";
+import AWS from 'aws-sdk';
+console.log("Loading function");
+
+export const handler = async (event, context) => {
+  let inbounds = createInboundCollection(event.Records);
+  try {
+    let sfcon = new SFConnect();
+    let result = await sfcon.postDataHelper("InboundQueue__c", inbounds);
+    console.log("sfdc result: %j", result);
+  } catch (error) {
+    console.log("error log == %j", error);
+  }
+  return `Successfully processed ${event.Records.length} records.`;
+}
+
+function createInboundCollection(records) {
+  let inbounds = [];
+  for (const record of records) {
+    console.log("DynamoDB Record: %j", record.dynamodb);
+    if (record.dynamodb.NewImage) {
+      let inboundObj = createinboundObj(record.dynamodb);
+      inbounds.push(inboundObj);
+    } else {
+      console.log("trigger new event not found! %j", record);
+    }
+  }
+  return inbounds;
+}
+
+function createinboundObj(evtdata) {
+  let inboundObj = {};
+  if (evtdata["NewImage"]) {
+    let record  = AWS.DynamoDB.Converter.unmarshall(evtdata.NewImage);
+    inboundObj = {
+      DataFormat__c: "JSON",
+      Endpoint__c:
+        "https://hjewgvy6f73p774zsrqtklygxu0lfpno.lambda-url.us-east-1.on.aws/",
+      ExternalId__c: record.id,
+      ObjectAPIName__c: "Order__c",
+      Payload__c: JSON.stringify(record.orderdata),
+      RecordId__c: record.externalId,
+      SourceSystem__c: "AWS",
+      Status__c: "Pending",
+      ownerId: "005B0000005GMN7IAO",
+      ExternalSequenceId__c: record.SequenceNumber,
+      //RequestLookup__c:(record.requestId.length <=18 ? record.requestId : null),
+      CorrelationId__c : record.requestId,
+      Channel__c : "Orders"
+    };
+    if(record.requestId.length <=18) {
+      inboundObj.RequestLookup__c = record.requestId;
+    }
+  }
+  console.log("inbound object : %j", inboundObj);
+  return inboundObj;
+}
---
+++ sfconnect.js
@@ -0,0 +1,69 @@
+/**
+ * @description       : 
+ * @author            : Ramanathan
+ * @group             : 
+ * @last modified on  : 09-18-2023
+ * @last modified by  : Ramanathan
+**/
+
+import jsforce from 'jsforce'
+import dotenv from "dotenv"
+dotenv.config();
+
+export default class SFConnect {
+     authInfo = {};
+    sfcon = null;
+
+    async init() {
+        //console.log("dotenv == %j", process.env); 
+        this.authInfo = await this.authWithUsernamePassword();
+        //console.log(`authinfo == ${JSON.stringify(this.authInfo)}`);
+        this.sfcon = await this.getsfconnection();
+        //console.log(`sfcon == ${this.sfcon}`);
+    }
+
+    async authWithUsernamePassword() {
+        const sfConnection = new jsforce.Connection({
+            loginUrl: process.env.SALESFORCE_LOGIN_URL
+        });
+        await sfConnection.login(
+            process.env.SALESFORCE_USERNAME,
+            process.env.SALESFORCE_PASSWORD+""+ process.env.SALESFORCE_TOKEN
+        );
+        return {
+            accessToken: sfConnection.accessToken,
+            instanceUrl: sfConnection.instanceUrl,
+            organizationId: sfConnection.userInfo.organizationId
+        }
+    }
+
+    async getsfconnection() {
+        this.sfcon = new jsforce.Connection({
+            instanceUrl : this.authInfo.instanceUrl,
+            accessToken : this.authInfo.accessToken
+          });
+        return this.sfcon;
+    }
+
+    async postData( sobj, data) {
+        try {
+            console.log("inbound records = %j", data)
+            let result = await this.sfcon.sobject(sobj).insert(data);
+            console.log(`result == ${JSON.stringify(result)}`)
+            return result;
+        } catch (error) {
+            console.log(`error == ${JSON.stringify(error)}`);
+        }
+    }
+
+    async postDataHelper(obj, data) {
+        await this.init();
+        return await this.postData (obj, data);
+    }
+}
+
+// let data = [{ Id : 'a01B000000FLsjnIAD', ERPOrderId__c : "testst"}];
+// let sfdcconnect = new SFConnect();
+// sfdcconnect.init();
+//  let result = sfdcconnect.postDataHelper("Order__c", data);
+// console.log(`result == ${JSON.stringify(result)}`);
